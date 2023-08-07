e3b27d9b12b34d48cc0ea92fd11b26ac4087e34b
Adam Partlow <adam.partlow@wales.nhs.uk>
2023-08-07 11:54:34 +0100

    Added open3d window with a layout.

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
+import open3d.visualization.gui as gui
+import open3d.visualization.rendering as rendering
+
+class PosturalIndexWindow():
+    def __init__(self, window_title: str, width: int, height: int) -> None:
+        self._window = gui.Application.instance.create_window(window_title, width, height)
+
+        w = self._window
+
+        self._scene = gui.SceneWidget()
+        self._scene.scene = rendering.Open3DScene(w.renderer)
+
+        em = w.theme.font_size
+        self._settings_panel = gui.Vert(0, gui.Margins(0.25 * em, 0.25 * em, 0.25 * em, 0.25 * em))
+
+        # A section where you can select which measurement to visualise
+        measurement_collapsable = gui.CollapsableVert("Measurement", 0.25 * em, gui.Margins(em, 0, 0, 0))
+        measurement_collapsable.add_child(gui.Label("Select a measurement"))
+        self._settings_panel.add_child(measurement_collapsable)
+
+        # Setup the panel on the right hand side and the scene on the left
+        w.set_on_layout(self._on_layout)
+        w.add_child(self._scene)
+        w.add_child(self._settings_panel)
+
+    # Callback to set the layout of the windows direct children
+    # See: http://www.open3d.org/docs/release/python_example/visualization/index.html#vis-gui-py
+    def _on_layout(self, layout_context):
+        r = self._window.content_rect
+        self._scene.frame = r
+        width = 17 * layout_context.theme.font_size
+        height = min(r.height, self._settings_panel.calc_preferred_size(layout_context, gui.Widget.Constraints()).height)
+        self._settings_panel.frame = gui.Rect(r.get_right() - width, r.y, width, height)
+
 def main():
-    print("Postural Index!")
+    # Initialise the application
+    gui.Application.instance.initialize()
+
+    # Setup the window
+    window = PosturalIndexWindow("Postural Index", 1920, 1080)
+
+    # Run event loop
+    gui.Application.instance.run()
 
 if __name__ == "__main__":
     main()
